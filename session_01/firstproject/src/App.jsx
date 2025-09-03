24e7107c7d2adddd22c47f302b60d754a5e87f4e
karthickag04 <karthickag04@gmail.com>
2025-09-03 18:04:29 +0530

    added session files of first react app

---
+++ session_01/firstproject/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App(props) {
+  return (
+    <div>
+      <h1>Hi .., {props.name}</h1>
+      <h1>Hi .., your age is {props.age}</h1>
+      <h1>Welcome To React</h1>
+    </div>
+  )
+}
+
+export default App
+
