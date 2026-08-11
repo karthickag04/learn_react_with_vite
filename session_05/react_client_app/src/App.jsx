fd40d9e5ac937bb062d0c6c7576b8ce2e96b8ca7
karthickag04 <karthickag04@gmail.com>
2026-08-11 18:43:12 +0530

    added react with registration

---
+++ session_05/react_client_app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Register from './Register'
+
+function App() {
+  
+
+  return (
+<>
+      <Register/>
+
+    </>
+  )
+}
+
+export default App
---
+++ session_05/react_client_app/src/Register.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Register(){
+
+const [user,setUser] = useState({
+    name:"",
+    email:"",
+    password:"",
+    role:"Student"
+});
+
+const handleChange = (e)=>{
+    setUser({
+        ...user,
+        [e.target.name]:e.target.value
+    });
+};
+
+const handleSubmit = async(e)=>{
+    e.preventDefault();
+
+    try{
+
+        const res = await axios.post(
+            "http://localhost:5000/api/users/register",
+            user
+        );
+
+        alert(res.data.message);
+
+    }
+    catch(error){
+        alert("Error registering user");
+    }
+};
+
+return(
+
+<div style={{width:"400px",margin:"auto"}}>
+
+<h2>User Registration</h2>
+
+<form onSubmit={handleSubmit}>
+
+<input
+type="text"
+name="name"
+placeholder="Name"
+onChange={handleChange}
+/>
+
+<br/><br/>
+
+<input
+type="email"
+name="email"
+placeholder="Email"
+onChange={handleChange}
+/>
+
+<br/><br/>
+
+<input
+type="password"
+name="password"
+placeholder="Password"
+onChange={handleChange}
+/>
+
+<br/><br/>
+
+<select name="role" onChange={handleChange}>
+
+<option value="Student">Student</option>
+<option value="Teacher">Teacher</option>
+<option value="Admin">Admin</option>
+
+</select>
+
+<br/><br/>
+
+<button type="submit">
+Register
+</button>
+
+</form>
+
+</div>
+
+);
+
+}
+
+export default Register;
